c0abb4b6aa5069b9ca5fb03bdd9c845ceae6b1da
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-10-02 13:03:46 -0700

    Answers and ex

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,48 @@
+'''python
+[ 1, -1, 2, 3, -4, -3, 4, -5, 6, 7 ] = [1, 3, 4]
+'''
+
 def has_negatives(a):
     """
     YOUR CODE HERE
     """
     # Your code here
+    # result = []
+    # for elem in a:
+    #     if elem < 0:
+    #         continue
+    #     else:
+    #         for num in a:
+    #             if num == -elem:
+    #                 result.append(elem)
+
+    result = []
+    d = {}
+    for elem in a:
+        if elem == 0:
+            continue
+        d[elem] = True
+        if -elem in d:
+            result.append(abs(elem))
+
+
 
     return result
+
+    # result = []
+    # d = {}
+    # for elem in a:
+    #     d[elem] = True
+    #     print(d[elem])
+    #     if -elem in d:
+    #         print(-elem)
+    #         if elem > 0:
+    #             result.append(elem)
+    #         else:
+    #             result.append(-elem)
+        
+
+    # return result
 
 
 if __name__ == "__main__":
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -9,12 +9,12 @@ def finder(files, queries):
     # Your code here
     result = []
     d = {}
-    for file_ in files:
-        last = file_.split('/')[-1]
+    for file_name in files:
+        last = file_name.split('/')[-1]
         if last in d:
-            d[last].append(file_)
+            d[last].append(file_name)
         else:
-            d[last] = [file_]
+            d[last] = [file_name]
     for qname in queries:
         if qname in d:
             result += d[qname]
