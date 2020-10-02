0424dd9788b436a9fb6dc6d40de046e01964b980
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-10-02 10:36:01 -0700

    Sprint challenge

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,6 +7,17 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    d = {}
+    for file_ in files:
+        last = file_.split('/')[-1]
+        if last in d:
+            d[last].append(file_)
+        else:
+            d[last] = [file_]
+    for qname in queries:
+        if qname in d:
+            result += d[qname]
 
     return result
 
